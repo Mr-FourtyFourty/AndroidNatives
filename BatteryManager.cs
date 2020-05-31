fbfee424de71d9113b1b0463bcb938db71f44693
Mr.FourtyFourty <65059656+Mr-FourtyFourty@users.noreply.github.com>
2020-05-31 22:50:39 +0530

    Add files via upload
    Gives Charging status and percentage

---
+++ BatteryManager.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public static class BatteryManager
+{
+    private static float percentage;
+    private static string status;
+
+
+    public static bool Charging()
+    {
+        status = SystemInfo.batteryStatus.ToString();
+        if (status == "Charging")
+        {
+            return true;
+        }
+        else //if (status == "Discharging")
+        {
+            return false;
+        }
+    }
+    public static float CurrentBatteryPercentage()
+
+    {
+        percentage = SystemInfo.batteryLevel * 100;
+        return percentage;
+    }
+}
+
